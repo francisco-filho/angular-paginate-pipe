173fd2d927d303ff664540371a02d481b16fe05e
F3445038 Francisco Manoel da Silva Filho <francisco.filho@bb.com.br>
2022-03-30 23:44:48 -0300

    refatorando pipe

--- src/app/paginate.pipe.ts
+++ src/app/paginate.pipe.ts
@@ -5,22 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PaginatePipe implements PipeTransform {
 
-  transform(value: string[], pagina: number= 0, total:number, totalPorPagina: number = 5):  string[]{
-    const slice = this.calcularItemInicialFinalDaPagina(pagina, total, totalPorPagina)
-    const result =  this.fatiarLista(value,slice.inicio, slice.fim)
-    console.log("result", total, totalPorPagina, result);
-    return result
+  transform(lista: string[], pagina: number= 0, total:number, totalPorPagina: number = 5):  string[]{
+    const pos = this.calcularItemInicialFinalDaPagina(pagina, totalPorPagina)
+    return lista.slice(pos.inicio, pos.fim) 
   }
 
-  calcularItemInicialFinalDaPagina(pagina: number, total: number, totalPorPagina: number){
-    const paginas = Math.floor(total / totalPorPagina);
+  calcularItemInicialFinalDaPagina(pagina: number, totalPorPagina: number){
     return {
       inicio: totalPorPagina * pagina,
       fim: (totalPorPagina * pagina) + totalPorPagina
     }
-  }
-
-  fatiarLista(lista: string[], inicio: number, fim: number){
-    return lista.slice(inicio, fim);
   }
 }
